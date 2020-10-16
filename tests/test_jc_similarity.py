5aab1f56e1f96f451e11bd312f8c0a3eec104605
Grupo Colav <grupocolav@udea.edu.co>
2020-10-16 16:41:10 +0200

    added vasic test

---
+++ tests/test_jc_similarity.py
@@ -0,0 +1,19 @@
+import unittest
+from hunahpu.similarity import jc_similarity
+
+class TestJC_similarity(unittest.TestCase):
+    def setUp(self):
+        self.title1 = "Implementation of industrial technology to improve the manufacturing process contoured seats [Implementación de la tecnología industrial para el mejoramiento del proceso de fabricación de asientos conformados]"
+        self.title2 = "Implementation of industrial technology to improve the manufacturing process contoured seats"
+        self.title3 = "Implementation of industrial technology to improve the manufacturing process contoured seats"
+
+
+    def test__working(self):
+        self.assertEqual(jc_similarity(self.title2,self.title3),True)
+
+    def test__failing(self):
+        self.assertNotEqual(jc_similarity(self.title1,self.title2), False)
+
+
+if __name__ == '__main__':
+    unittest.main()
