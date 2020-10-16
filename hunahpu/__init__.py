6514e300a9f96f9e3ed09484b1befc90acf33d80
Grupo Colav <grupocolav@udea.edu.co>
2020-10-16 15:41:18 +0200

    added code for similarity and package skeleton

---
+++ hunahpu/__init__.py
@@ -0,0 +1,1 @@
+from hunahpu import similarity
---
+++ setup.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+# Copyright (c) Colav.
+# Distributed under the terms of the Modified BSD License.
+
+# -----------------------------------------------------------------------------
+# Minimal Python version sanity check (from IPython)
+# -----------------------------------------------------------------------------
+
+# See https://stackoverflow.com/a/26737258/2268280
+# sudo pip3 install twine
+# python3 setup.py sdist bdist_wheel
+# twine upload dist/*
+# For test purposes
+# twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+
+from __future__ import print_function
+from setuptools import setup, find_packages
+
+import os
+import sys
+
+
+v = sys.version_info
+
+shell = False
+if os.name in ('nt', 'dos'):
+    shell = True
+    warning = "WARNING: Windows is not officially supported"
+    print(warning, file=sys.stderr)
+
+
+def main():
+    setup(
+        # Application name:
+        name="Hunahpu",
+
+        # Version number (initial):
+        version="0.0.1-alpha",
+
+        # Application author details:
+        author="Colav",
+        author_email="grupocolav@udea.edu.co",
+
+        # Packages
+        packages=find_packages(exclude=['tests']),
+
+        # Include additional files into the package
+        include_package_data=True,
+
+        # Details
+        url="https://github.com/colav/Hunahpu",
+
+        #
+        license="MIT",
+
+        description="Similarity metrics for bibliography",
+
+        long_description=open("README.md").read(),
+
+        long_description_content_type="text/markdown",
+
+        # Dependent packages (distributions)
+        install_requires=[
+            'nltk==3.5'
+        ],
+    )
+
+
+if __name__ == "__main__":
+    main()
