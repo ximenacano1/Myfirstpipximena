a354ce2fd41f9b6d2f643d5422867c0c1f10787d
Grupo Colav <grupocolav@udea.edu.co>
2020-10-17 12:32:26 +0200

    fixed division by zero on jc_similarity and implemented our custom similarity metric

--- hunahpu/similarity.py
+++ hunahpu/similarity.py
@@ -1,8 +1,34 @@
 from nltk import ngrams
 from unidecode import unidecode
+from fuzzywuzzy import fuzz
+from langid import classify
+from googletrans import Translator
 
 
-def jc_similarity(title1, title2, n=3, boolean=False, threshold=0.8):
+def __jc_similarity_base(title1, title2, n=3):
+    '''
+    Utility function to avoid code duplication, implementation below.
+    '''
+    n1 = ngrams(title1.split(), n)
+    n2 = ngrams(title2.split(), n)
+    n1 = set(n1)
+    n2 = set(n2)
+    unilen = len(n1.union(n2))
+    lenmin = min([len(n1), len(n2)])
+
+    if unilen == 0 or lenmin == 0:
+        S = 0
+    else:
+        J = len(n1.intersection(n2))/unilen
+        C = len(n1.intersection(n2))/lenmin
+        if J+C != 0:
+            S = 2*J*C/(J+C)
+        else:
+            S = 0
+    return S
+
+
+def jc_similarity(title1, title2, n=3, boolean=False, threshold=0.8, use_traslation=True):
     '''
     Computes titles similarity according to section 2.6 of
     https://arxiv.org/pdf/1911.02782.pdf
@@ -19,6 +45,8 @@ def jc_similarity(title1, title2, n=3, boolean=False, threshold=0.8):
         If you want the output to be a bool
     threshold : float
         Threshold for the boolean outpput
+    use_traslation : str
+        enable translation support
 
     Returns
     -------
@@ -28,13 +56,21 @@ def jc_similarity(title1, title2, n=3, boolean=False, threshold=0.8):
     '''
     title1 = unidecode(title1.lower())
     title2 = unidecode(title2.lower())
-    n1 = ngrams(title1.split(), n)
-    n2 = ngrams(title2.split(), n)
-    n1 = set(n1)
-    n2 = set(n2)
-    J = len(n1.intersection(n2))/len(n1.union(n2))
-    C = len(n1.intersection(n2))/min([len(n1), len(n2)])
-    S = 2*J*C/(J+C)
+
+    S = __jc_similarity_base(title1, title2, n)
+
+    if S < threshold and use_traslation:
+        lang, _ = classify(title1)
+        if lang != "en":
+            translator = Translator()
+            title1 = translator.translate(title1).text
+        lang, _ = classify(title2)
+        if lang != "en":
+            translator = Translator()
+            title2 = translator.translate(title2).text
+
+    S = __jc_similarity_base(title1, title2, n)
+
     if boolean:
         if S >= threshold:
             return True
@@ -42,3 +78,58 @@ def jc_similarity(title1, title2, n=3, boolean=False, threshold=0.8):
             return False
     else:
         return S
+
+
+def __colav_similarity(title1, title2, upper_thold=90, bottom_thold=40):
+    '''
+    Utility function to avoid code duplication, public implementation below.
+    '''
+    label = False
+    ratio = fuzz.partial_ratio(title1, title2)
+    if ratio > upper_thold:
+        label = True
+    elif ratio > upper_thold:
+        ratio = fuzz.token_set_ratio(title1, title2)
+        if ratio > bottom_thold:
+            label = True
+        elif ratio > bottom_thold:
+            ratio = fuzz.partial_token_set_ratio(title1, title2)
+            if ratio > upper_thold:
+                label = True
+    return label
+
+
+def colav_similarity(title1, title2, upper_thold=90, bottom_thold=40, use_traslation=True):
+    '''
+    custom metric for similarity using multiple nested metrics from fuzzywuzzy
+
+    Parameters
+    ----------
+    upper_thold: int
+        upper threshold for partial ratio and token_set_ratio
+    bottom_thold: int
+        bottom threshold for partial ratio and token_set_ratio
+    use_traslation: boolean
+        support similarity with translation
+    '''
+    title1 = unidecode(title1.lower())
+    title2 = unidecode(title2.lower())
+
+    label = __colav_similarity(title1, title2, upper_thold, bottom_thold)
+    if label is False and use_traslation is True:
+        translated = 0
+        lang, _ = classify(title1)
+        if lang != "en":
+            translator = Translator()
+            title1 = translator.translate(title1).text
+            translated = True
+        lang, _ = classify(title2)
+        if lang != "en":
+            translator = Translator()
+            title2 = translator.translate(title2).text
+            translated = True
+        if translated is True:
+            label = __colav_similarity(
+                title1, title2, upper_thold, bottom_thold)
+
+    return label
--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ def main():
         name="Hunahpu",
 
         # Version number (initial):
-        version="0.0.1-alpha",
+        version="0.0.2-alpha",
 
         # Application author details:
         author="Colav",
@@ -64,7 +64,10 @@ def main():
         # Dependent packages (distributions)
         install_requires=[
             'nltk==3.5',
-            'Unidecode==1.1.1'
+            'Unidecode==1.1.1',
+            'fuzzywuzzy==0.18.0',
+            'langid==1.1.6',
+            'translate==3.5.0',
         ],
     )
 
