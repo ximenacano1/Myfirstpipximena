4957130d068499ba591940945beebba858dcc1e8
Diego Restrepo <restrepo@udea.edu.co>
2020-11-01 22:26:02 -0500

    lc

--- tests/test_anomalies.py
+++ tests/test_anomalies.py
@@ -3,8 +3,8 @@ import numpy as np
 from anomalies import anomaly
 class TestColav_anomaly(unittest.TestCase):
     def test__working(self):
-        self.assertEqual(anomaly.free([-1,1],[4,-2]),
-                         np.asarray( [3, 3, 3, -12, -12, 15]  ), True)
+        self.assertEqual( list( anomaly.free([-1,1],[4,-2]) ),
+                          [3, 3, 3, -12, -12, 15]  , True)
 
 if __name__ == '__main__':
     unittest.main()
